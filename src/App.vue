02575c1c7704bbcafcea37fc3bf7fbe82f373f62
kevinlin1168 <kevinlin1168@gmail.com>
2019-08-21 21:53:34 +0800

    190821 modify timeout function

--- src/App.vue
+++ src/App.vue
@@ -19,17 +19,17 @@ export default {
   },
   methods: {
     OperatingWebsite() {
-      let currentTime = this.currentTime;
-      let lastTime = new Date().getTime();
+      let lastTime = this.currentTime;
+      let currentTime = new Date().getTime();
       let timeOut = 60 * 60 * 1000; //設定時間 60分鐘
-      console.warn(lastTime - currentTime > timeOut)
-      if (lastTime - currentTime > timeOut) {
-        this.currentTime = new Date().getTime(); 
-      } else {
+      if ((currentTime - lastTime) > timeOut) {
+        console.warn('timeOut')
         // 未操作頁面，跳轉登入頁面
         this.currentTime = new Date().getTime(); 
         sessionStorage.setItem('isLogin', false);
         this.$router.push('/');
+      } else {
+        this.currentTime = new Date().getTime(); 
       }
     }
   }
